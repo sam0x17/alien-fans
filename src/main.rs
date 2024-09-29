a0fa8b4479fc2b6d5d607653030cd77d208a376f
Sam Johnson <sam@durosoft.com>
2024-09-28 23:53:04 -0400

    tweak

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,7 @@ fn find_hwmon_device(device_name: &str) -> Option<PathBuf> {
             let hwmon_name_file = entry.path().join("name");
 
             if let Ok(content) = fs::read_to_string(hwmon_name_file) {
+                println!("Found hwmon device: {}", content.trim());
                 if content.trim().contains(device_name) {
                     return Some(entry.path());
                 }
